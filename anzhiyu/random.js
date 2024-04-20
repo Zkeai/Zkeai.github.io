a54b23123fcd988c3575b1b83855d11c007f022a
yobo <saorenzq@gmail.com>
2024-04-21 00:14:14 +0800

    Site updated: 2024-04-21 00:14:14

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["posts/66de.html","posts/ffe6.html","posts/ff8d.html","posts/87ae.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["posts/66de.html","posts/87ae.html","posts/ffe6.html","posts/ff8d.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
