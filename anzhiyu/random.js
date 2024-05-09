6f3ff1abeeaa4358a5aaa1fcedb26e8f5038b983
yobo <saorenzq@gmail.com>
2024-05-09 10:55:19 +0800

    Site updated: 2024-05-09 10:55:19

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["posts/f6ac.html","posts/66de.html","posts/ffe6.html","posts/ff8d.html","posts/87ae.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["posts/f6ac.html","posts/66de.html","posts/ffe6.html","posts/ff8d.html","posts/1795.html","posts/87ae.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
